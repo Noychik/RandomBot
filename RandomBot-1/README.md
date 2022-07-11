1752a156d069488b10b6022f92a76cd58fad38c1
Noychik <108848870+Noychik@users.noreply.github.com>
2022-07-11 22:43:31 +0700

    Add files via upload

---
+++ RandomBot-1/README.md
@@ -0,0 +1,5 @@
+<h1>Random Bot</h1>
+<p>Hi! This is random bot for Telegram!</p>
+<h3>Contributors:</h3>
+<li><a href="https://github.com/NoLupiPls">NoLupiPls</a> <i>Ilya</i>
+<li><a href="https://github.com/Noychik">Noychik</a> <i>Nazar</i>
--- RandomBot-1/main.py
+++ RandomBot-1/main.py
@@ -1,3 +1,4 @@
+from email.errors import NoBoundaryInMultipartDefect
 import logging
 import sqlite3
 from aiogram import Bot, Dispatcher, executor, types
@@ -6,37 +7,75 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import random
 
-bot = Bot(token="5477947327:AAFYzuSSYKNt7fXbY6imbK202rdDHdST-Yk")
+#   TOKEN   #
+bot = Bot(token="5385406422:AAGnCUR7gRZ8xTZgr8rdRY4NIXbktJORlNg")
 dp = Dispatcher(bot, storage=MemoryStorage())
 logging.basicConfig(level=logging.INFO)
 
+
+#   CONNECT  SQL   #
 conn = sqlite3.connect('db/db.db', check_same_thread=False)
 cursor = conn.cursor()
 
-def db_tab_val(user_id: int, user_name: str, user_surname: str, username: str):
-    cursor.execute('INSERT INTO test (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)', (user_id, user_name, user_surname, username))
+def db_tab_val(user_id: int, user_name: str, user_surname: str, username: str, us_starting:str):
+    cursor.execute('INSERT INTO test (user_id, user_name, user_surname, username, us_starting) VALUES (?, ?, ?, ?, ?)', (user_id, user_name, user_surname, username, us_starting))
     conn.commit()
+def upd_tab_val(user_id, us_starting):
+    cursor.execute("UPDATE `test` SET `us_starting` = ? WHERE `user_id` = ?", (us_starting, user_id(user_id)))
+    return conn.commit()
 
+
+#      VARIABLE      #
 a = 0
-sum = 6
+xr = 0
+nufl = None
+
+
+#      MESSAGE      #
 @dp.message_handler(commands=["start"])
 async def welcome(message: types.Message):
-    await message.answer("Привет! Здесь ты можешь узнать сколько у тя см хуй"
-                         "\n По команде /check")
+    buttons = [
+        types.InlineKeyboardButton(text="Добавить бота в группу", url="http://t.me/djdbddjdiekemehzoakwbot?startgroup=Lichka")
+    ]
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    keyboard.add(*buttons)
+
+    nufl = 1
+    
+    await message.answer("Привет! Здесь ты можешь узнать сколько у тебя хромосом"
+                         "\nОт -5, до +10 за одну команду"
+                         "\nПо команде /check", reply_markup=keyboard)
+    
+    
     us_id = message.from_user.id
     us_name = message.from_user.first_name
     us_sname = message.from_user.last_name
     username = message.from_user.username
 
     db_tab_val(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
-    
+
 @dp.message_handler(commands=["check"])
 async def volume(message: types.Message):
-    
-    rand = int(random.randint(-15, 15))
-    sum = str(rand + a)
-    
-    await message.answer("Вот твой хуёк = " + sum)
+    global xr
+    rand = int(random.randint(-5, 10))
+    su = rand + a + int(xr)
+    #      FIRST   NAME     #   
+    if rand == 0:
+        xr = str(su)
+        await message.answer(f"{message.from_user.first_name}, твоё количство хромосом не изменилось, их количество равно " + xr + ' шт')
+    elif rand > 0:
+        xr = str(su)
+        await message.answer(f"{message.from_user.first_name}, твоё количство хромосом увеличилось, их количество равно " + xr + ' шт')
+    elif rand < 0:
+        xr = str(su)
+        await message.answer(f"{message.from_user.first_name}, твоё количство хромосом уменьшилось, их количество равно " + xr + ' шт')
+
+    us_id = message.from_user.id
+    xr = str(su)
+    usvol = xr
+    upd_tab_val(user_id=us_id, us_starting=usvol)
+
+
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
